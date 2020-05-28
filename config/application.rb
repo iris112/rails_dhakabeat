3848d3a93927e5eeb99e26e90e00130f1520e958
zhdanov2020818 <zhuangjin@docyt.com>
2020-05-28 09:46:19 +0300

    make two pages

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Myapp
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.available_locales = [:en, :ru, :de]
-    config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     root 'pages#index'
+    get 'about', to: 'pages#about'
   end
 end
