575d54820c0e617bf32dfeaf8fa9373d3165aec3
zhdanov2020818 <zhuangjin@docyt.com>
2020-05-29 17:12:44 +0300

    change file structure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_locale
+  private
+
+  def set_locale
+    I18n.locale = extract_locale || I18n.default_locale
+  end
+  def extract_locale
+    parsed_locale = params[:locale]
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
+
+  private
+  def default_url_options
+    { locale: I18n.locale }
+  end
+  
 end
